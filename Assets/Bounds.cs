148b53477046edcaadbff910538a888984384e50
Alexis Bechet <fervac01@gmail.com>
2019-10-22 14:16:57 +0200

    Initial commit

---
+++ Assets/Bounds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<Ball>().Respawn();
+        FindObjectOfType<BrickManager>().ResetLevel();
+    }
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 5f;
+    private float input;
+
+    void Update()
+    {
+        input = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+      GetComponent<Rigidbody2D>().velocity = Vector2.right * input * speed;
+    }
+}
